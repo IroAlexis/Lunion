9c9a5c5c10c6af9c49e345e7a8af0c3d8723d615
IroAlexis <iroalexis@outlook.fr>
2021-04-15 11:42:27 +0200

    [tests] Test the functions allowing to manipulate the environment variables

---
+++ tests/test-system.c
@@ -0,0 +1,121 @@
+/* test-system.c
+ *
+ * Copyright (C) 2020-2021 Alexis Peypelut <peypeluta@live.fr>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "../src/system.h"
+
+
+#define ANSI_COLOR_RED   "\x1b[31m"
+#define ANSI_COLOR_GREEN "\x1b[32m"
+#define ANSI_COLOR_RESET "\x1b[0m"
+
+
+
+int test_lunion_set_env_var (const char* name, const char* value)
+{
+	lunion_set_env_var (name, value);
+
+	fprintf (stderr, "[+] test:: lunion_set_env_var: ");
+	if (getenv (name) == NULL)
+	{
+		fprintf (stderr, ANSI_COLOR_RED "FAILED\n" ANSI_COLOR_RESET);
+		return EXIT_FAILURE;
+	}
+
+	fprintf (stderr, ANSI_COLOR_GREEN "DONE\n" ANSI_COLOR_RESET);
+	return EXIT_SUCCESS;
+}
+
+
+int test_error_lunion_set_env_var (const char* name, const char* value)
+{
+	lunion_set_env_var (name, value);
+
+	fprintf (stderr, "[+] test_err:: lunion_set_env_var: ");
+	if (getenv (name) != NULL)
+	{
+		fprintf (stderr, ANSI_COLOR_RED "FAILED\n" ANSI_COLOR_RESET);
+		return EXIT_FAILURE;
+	}
+
+	fprintf (stderr, ANSI_COLOR_GREEN "DONE\n" ANSI_COLOR_RESET);
+	return EXIT_SUCCESS;
+}
+
+
+int test_lunion_get_env_var (const char* name)
+{
+	char* value = getenv (name);
+
+	fprintf (stderr, "[+] test:: lunion_get_env_var: ");
+	if (value == NULL)
+	{
+		fprintf (stderr, ANSI_COLOR_RED "FAILED\n" ANSI_COLOR_RESET);
+		return EXIT_FAILURE;
+	}
+
+	fprintf (stderr, ANSI_COLOR_GREEN "DONE\n" ANSI_COLOR_RESET);
+	return EXIT_SUCCESS;
+}
+
+
+int test_error_lunion_get_env_var (const char* name)
+{
+	char* value = getenv (name);
+
+	fprintf (stderr, "[+] test_err:: lunion_get_env_var: ");
+	if (value != NULL)
+	{
+		fprintf (stderr, ANSI_COLOR_RED "FAILED\n" ANSI_COLOR_RESET);
+		return EXIT_FAILURE;
+	}
+
+	fprintf (stderr, ANSI_COLOR_GREEN "DONE\n" ANSI_COLOR_RESET);
+	return EXIT_SUCCESS;
+}
+
+
+int test_lunion_unset_env_var (const char* name)
+{
+	lunion_unset_env_var (name);
+
+	fprintf (stderr, "[+] test:: lunion_unset_env_var: ");
+	if (getenv (name) != NULL)
+	{
+		fprintf (stderr, ANSI_COLOR_RED "FAILED\n" ANSI_COLOR_RESET);
+		return EXIT_FAILURE;
+	}
+
+	fprintf (stderr, ANSI_COLOR_GREEN "DONE\n" ANSI_COLOR_RESET);
+	return EXIT_SUCCESS;
+}
+
+
+int main ()
+{
+	test_lunion_set_env_var ("WINEDLLOVERRIDES", "\"mscoree,mshtml,winemenubuilder.exe=\"");
+	test_error_lunion_set_env_var ("WINEDEBUG=", "-all");
+
+	test_lunion_get_env_var ("WINEDLLOVERRIDES");
+	test_error_lunion_get_env_var ("WINEDEBUG=");
+
+	test_lunion_unset_env_var ("WINEDLLOVERRIDES");
+}
