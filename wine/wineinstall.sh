09dbb0915303079986d1aa7391afee91d2a177a4
IroAlexis <iroalexis@outlook.fr>
2021-06-01 20:39:10 +0200

    [wine] Implement games installation

--- wine/wineinstall.sh
+++ wine/wineinstall.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env bash
 
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 game_name"
+if [ $# -gt 2 ]; then
+	echo "Usage: $0 game_name [/path/to/setup_exe]"
 	exit 1
 fi
 
+if [ -e $1 ]; then
+	echo "game_name ($1) don't must not be a path"
+	exit 1
+fi
 if [ ! -d $HOME/Games/$1 ]; then
 	mkdir -p $HOME/Games/$1
 fi
-
 export GAMEDIR=$HOME/Games/$1
 
 
@@ -47,9 +50,42 @@ if [ $? ]; then
 
 	read -rp "Do you want install VKD3D Proton for Direct3D 12 (N/y) ? " _CONDITION;
 	if [[ "$_CONDITION" =~ [yY] ]]; then
-		mkdir -p $GAMEDIR/shaders
-
 		$HOME/runtime/vkd3d-proton-master/setup_vkd3d_proton.sh install && wineserver --wait
+	fi
+
+
+	if [ $2 ]; then
+		if [ ! -f $2 ]; then
+			exit 1
+		fi
+
+		read -rp "Is it a GOG installer (n/y) ? " _CONDITION
+		if [[ "$_CONDITION" =~ [yY] ]]; then
+			read -rp "Do you want a gui (n/y) ? " _CONDITION
+			if [[ "$_CONDITION" =~ [nN] ]]; then
+				ARGS="/SP- /SUPPRESSMSGBOXES"
+
+				read -rp "Do you want a silent installation without progress bar (n/y) ? " _CONDITION
+				if [[ "$_CONDITION" =~ [yY] ]]; then
+					ARGS+=" /VERYSILENT"
+				else
+					ARGS+=" /SILENT"
+				fi
+
+				read -rp "Do install the game outside the wine prefix (n/y) ? " _CONDITION
+				if [[ "$_CONDITION" =~ [yY] ]]; then
+					mkdir -p $GAMEDIR/gamedata
+
+					ARGS+=" DIR=\"$(winepath -w $GAMEDIR/gamedata)\""
+				fi
+			fi
+		fi
+
+		if [ ! -z "$ARGS" ]; then
+			wine $2 $ARGS && wineserver --wait
+		else
+			wine "$2" && wineserver --wait
+		fi
 	fi
 
 	exit 0
